32ba88770b3f80f4f2df778e5455f58770a2ba56
Miguel <miguelangelodeleonl12@gmail.com>
2022-09-05 21:05:22 +0200

    feat: added eventListener

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const choices = ['+', '-', '*', '/', '7', '8', '9', '4', '5', '6', '1', '2', '3', '0', '.', 'C', '=']
+const inputContainer = document.querySelector('.calculator__inputs')
 
-const inputs = document.querySelector('.calculator__inputs')
-
-choices.map(item => {
-    const input = document.createElement('div')
+//render boxes
+choices.forEach(item => {
+    const input = document.createElement('button')
     item === '=' ? input.classList.add('input', 'equals') :input.classList.add('input')
-    input.innerHTML = `<p>${item}</p>`
-    inputs.appendChild(input)
+    input.innerHTML = item
+    inputContainer.appendChild(input)
 })
+
+//written inputs here because of rendering above
+const inputs = document.querySelectorAll('.input')
+
+handleInputClick = (e) =>{
+    console.log(e.target.textContent)
+}
+
+inputs.forEach(input => {
+    input.addEventListener('click', handleInputClick)
+});
